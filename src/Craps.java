21d591b5501c2d0237160fa555e96f7512bbdd85
Mike Knoles <MKnoles78@gmail.com>
2021-11-27 22:33:42 -0500

    added constants

--- src/Craps.java
+++ src/Craps.java
@@ -1,5 +1,23 @@
 
+// Fig. 6.8: Craps.java
+// Craps class simulates the dice game craps
+import java.security.SecureRandom;
+
 public class Craps {
+	// create secure random number generator for use in method rollDice
+	private static final SecureRandom randomNumbers = new SecureRandom();
+
+	// enum type with constants that represent the game status
+	private enum Status {
+		CONTINUE, WON, LOST
+	};
+
+	// constants that represent common rolls of the dice
+	private static final int SNAKE_EYES = 2;
+	private static final int TREY = 3;
+	private static final int SEVEN = 7;
+	private static final int YO_LEVEN = 11;
+	private static final int BOX_CARS = 12;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
